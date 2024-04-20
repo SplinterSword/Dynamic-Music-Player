01e3c814ed80150043a6368ed28d91fbbb16fc7b
SplinterSword <57327315+SplinterSword@users.noreply.github.com>
2024-04-21 00:31:13 +0530

    SpotifyClone

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 console.log("Lets write javascript")
 
+let currentSong = new Audio();
+
 async function getSongs(){
     let a = await fetch("http://127.0.0.1:5500/songs/");
     let response = await a.text();
@@ -17,16 +19,35 @@ async function getSongs(){
 }
 
 const playMusic = (track,artist)=>{
-    let audio = new Audio("/songs/" + track + "-" + artist + ".mp3");
-    audio.play();
+    currentSong.src  = "/songs/" + track + "-" + artist + ".mp3";
+    currentSong.play();
+    play.src = "pause.svg"
+    document.querySelector(".songinfo").innerHTML = track + "-" + artist;
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
 }
 
 async function main(){
-
-    let currrentSong;
-
     // Get the list of all the songs
     let songs = await getSongs();
+    currentSong.src = "/songs/" + "Dreamer" + "-" + "Roa" + ".mp3";
+    document.querySelector(".songinfo").innerHTML = "Dreamer" + "-" + "Roa";
+    document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/03:15`;
+
+    //seconds to minutes
+    function secondsToMinutesSeconds(seconds) {
+        // Calculate the minutes and remaining seconds
+        let minutes = Math.floor(seconds / 60);
+        let remainingSeconds = Math.floor(seconds % 60);
+    
+        // Add leading zeros if necessary
+        let minutesString = minutes < 10 ? '0' + minutes : minutes;
+        let secondsString = remainingSeconds < 10 ? '0' + remainingSeconds : remainingSeconds;
+    
+        // Combine minutes and seconds with a colon
+        let formattedTime = minutesString + ':' + secondsString;
+    
+        return formattedTime;
+    }
 
     //Show All the Songs in the playlist
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
@@ -50,6 +71,34 @@ async function main(){
         e.addEventListener("click",element=>{
             playMusic(e.querySelector(".info").childNodes[1].innerHTML.trim(),e.querySelector(".info").childNodes[3].innerHTML.trim());
         })
+    });
+
+    // Attach an even listener to play, next and previous
+
+    //Play
+    let play = document.getElementById("play");
+    play.addEventListener("click", ()=>{
+        if(currentSong.paused){
+            currentSong.play();
+            play.src = "pause.svg"
+        }
+        else{
+            currentSong.pause()
+            play.src = "play_mediaplayer.svg"
+        }
+    });
+
+    // Listen for timeupdate event
+    currentSong.addEventListener("timeupdate",()=>{
+        document.querySelector(".songtime").innerHTML = `${secondsToMinutesSeconds(currentSong.currentTime)}/${secondsToMinutesSeconds(currentSong.duration)}`;
+        document.querySelector(".circle").style.left = (currentSong.currentTime/currentSong.duration)*100 + "%";
+    });
+
+    // Add an event listener to seek bar
+    document.querySelector(".seekbar").addEventListener("click",e=>{
+        let percent = (e.offsetX/e.target.getBoundingClientRect().width)*100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = currentSong.duration * (percent/100);
     })
 };
 
